bde2c87adb68bccc294242751b91687851a5ca74
Gret-app <greta.mik85@gmail.com>
2025-01-13 09:16:40 +0100

    Fixed bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import babelParser from "@babel/eslint-parser";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+export default {
+  languageOptions: {
+    parser: babelParser,
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": ["warn"],
+  },
+};
