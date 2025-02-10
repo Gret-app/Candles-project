1e04b38048682fc9503111152f98062ffe44621c
Gret-app <greta.mik85@gmail.com>
2025-02-10 18:12:32 +0100

    Improved responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/logo.png";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
   return (
     <div className="navbar">
+      {/* Logo i tytuł */}
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1 className="site-title">Pure Aura</h1>
+      </div>
+
+      {/* Hamburger menu */}
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
       {/* Linki nawigacyjne */}
-      <div className="nav-links">
-        <NavLink
-          to="/"
-          end
-          className={({ isActive }) => (isActive ? "active-link" : "")}
-        >
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <NavLink to="/" onClick={closeMenu}>
           Strona główna
         </NavLink>
-        <NavLink
-          to="/bestsellery"
-          className={({ isActive }) => (isActive ? "active-link" : "")}
-        >
+        <NavLink to="/bestsellery" onClick={closeMenu}>
           Bestsellery
         </NavLink>
-        <NavLink
-          to="/oferta"
-          className={({ isActive }) => (isActive ? "active-link" : "")}
-        >
+        <NavLink to="/oferta" onClick={closeMenu}>
           Oferta
         </NavLink>
-        <NavLink
-          to="/dodaj"
-          className={({ isActive }) => (isActive ? "active-link" : "")}
-        >
+        <NavLink to="/dodaj" onClick={closeMenu}>
           Dodaj produkt
         </NavLink>
       </div>
 
-      {/* Logo i tytuł */}
-      <div className="navbar-left">
-        <Link to="/" className="logo-link">
-          <img src={logo} alt="Logo sklepu" className="logo" />
-          <h1 className="site-title">Pure Aura</h1>
-        </Link>
-      </div>
-
-      {/* Social Media */}
+      {/* Social media */}
       <div className="social-media">
         <a
           href="https://facebook.com"
